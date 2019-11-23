1d3308bbd8f55da91e4e49592deacf52ed8e3b66
Laurens Devos <laurens.devos@outlook.com>
2019-11-23 13:32:42 +0100

    dask parallel solver wip

--- src/python/treeck/parallel_solver.py
+++ src/python/treeck/parallel_solver.py
@@ -2,9 +2,9 @@ import dask
 
 class ParallelSolver:
 
-    def __init__(self, client, sp):
-        self._client = client # dask client
-        self._sp = sp # SearchSpace
+    def __init__(self, client, addtree):
+        self.client = client # dask client
+        self.addtree = addtree # SearchSpace
 
     def verify(self):
         pass
@@ -13,6 +13,25 @@ class ParallelSolver:
         #results = dask.compute(tasks)
         #print(results)
         #print(self._trees._reachable)
+
+    def _step1(self):
+        """
+        Construct a few complex trees for each worker.
+        """
+        nthreads = client.nthreads()
+        nleafs = nthreads
+
+        sp = SearchSpace(self.addtree)
+        sp.split(nleafs)
+        num_features = sp.num_features()
+
+        tasks = []
+        for leaf in sp.leafs():
+            addtree = sp.get_pruned_addtree(leaf).to_json()
+            domains = sp.get_domains(leaf)
+            tasks.append((domains, addtree))
+
+
 
 
         
