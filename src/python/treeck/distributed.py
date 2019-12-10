9a46a7ab5e65b2fbe151ea4ba512dce85370568c
Laurens Devos <laurens.devos@outlook.com>
2019-12-10 09:56:28 +0100

    distributed verifying

--- src/python/treeck/distributed.py
+++ src/python/treeck/distributed.py
@@ -1,14 +1,15 @@
 import os, socket, codecs, random, time
 from enum import Enum
-from dask.distributed import wait, as_completed, get_client, secede, rejoin, fire_and_forget, worker_client
+from dask.distributed import wait, get_client
 
 from .pytreeck import SearchSpace, AddTree
+from .verifier import Verifier
 
 class DistributedVerifier:
 
-    #class TaskType(Enum):
-    #    SPLIT,
-    #    VERIFY
+    class TaskType(Enum):
+        SPLIT = 1
+        VERIFY = 2
 
     def __init__(self, client, addtree, verifier_factory):
         self._client = client # dask client
@@ -16,138 +17,77 @@ class DistributedVerifier:
         self._verifier_factory = verifier_factory # (domains, addtree) -> Verifier
         self._branch_factor = sum(client.nthreads().values()) * 4
 
-    #def run(self):
-    #    c = self._client
-    #    timeout = 60
-    #    futures0 = [c.submit(DistributedVerifier._split_fun,
-    #        None, self._addtree, self._branch_factor)]
-    #    futures1 = []
-
-    #    # maybe work with handlers instead of as_completed
-    #    while len(futures0) > 0:
-    #        for future in as_completed(futures0):
-    #            tup = future.result()
-    #            typ = tup[0]
-    #            if typ == DistributedVerifier.TaskType.SPLIT:
-    #                futures1 += self._handle_split_result(*tup[1:], timeout)
-    #            elif typ == DistributedVerifier.TaskType.SPLIT:
-    #                futures1 += self._handle_verify_result(*tup[1:])
-    #            else:
-    #                raise RuntimeError("unknown TaskType")
-
-    #        timeout *= 1.2
-    #        futures0 = futures1
-    #        futures1 = []
-
     def run(self):
-        #fs = [self._client.submit(DistributedVerifier.test2, 1) for i in range(10)]
+        at_enc = DistributedVerifier._enc_at(self._addtree)
+        f = self._client.submit(DistributedVerifier._split_fun,
+                None, at_enc, self._branch_factor)
+        fs = [f]
 
-        #for f in as_completed(fs):
-        #    print("[4]   bye from", f.result(), os.getpid() % 10)
-
-        fs = [self._client.submit(DistributedVerifier.test2, 1)]
-
-        while len(fs) > 0:
-            print(len(fs), "=================")
+        while len(fs) > 0: # while task are running...
             wait(fs, return_when="FIRST_COMPLETED")
+
             next_fs = []
             for f in fs:
                 if f.done():
-                    i, new_fs = f.result()
-                    if new_fs is not None:
-                        if isinstance(new_fs, list):
-                            print("list", i)
-                            for new_f in new_fs:
-                                next_fs.append(new_f)
-                        else:
-                            print("single", i)
-                            next_fs.append(new_fs)
-                    else:
-                        print("done", i)
+                    next_fs += self._handle_done_future(f)
                 else:
                     next_fs.append(f)
             fs = next_fs
+
         print("done!")
 
-    @staticmethod
-    def test2(i):
-        print("hallo!", i, os.getpid() % 10)
-        time.sleep(1.0)
-        if i < 10:
-            client = get_client()
-            if i % 2 == 1:
-                return i, client.submit(DistributedVerifier.test2, i+1)
-            else:
-                return i, [client.submit(DistributedVerifier.test2, i+j+1) for j in range(2)]
-        return i, None
+    def _handle_done_future(self, future):
+        assert future.done()
+        result = future.result()
+        if result[0] == DistributedVerifier.TaskType.SPLIT:
+            subprobs = result[1]
+            return self._handle_split_result(subprobs)
+        if result[0] == DistributedVerifier.TaskType.VERIFY:
+            status, doms, at_enc = result[1:]
+            return self._handle_verify_result(status, doms, at_enc)
 
-    #def run(self):
-    #    print("hello??")
-    #    print("running", os.getpid())
-    #    at_enc = DistributedVerifier._enc_at(self._addtree)
-    #    f = self._submit_split_task(None, at_enc)
-    #    #print("result: ", f.result())
+    def _handle_split_result(self, subprobs):
+        print("SPLIT HANDLER with", len(subprobs), "subprobs")
+        timeout = 10
+        fs = []
+        for doms, at_enc in subprobs:
+            f = self._client.submit(DistributedVerifier._verify_fun,
+                    self._verifier_factory, doms, at_enc, timeout)
+            fs.append(f)
+        return fs
 
-    #def _submit_split_task(self, doms, at_enc):
-    #    future = self._client.submit(DistributedVerifier._split_fun,
-    #            doms, at_enc, self._branch_factor)
-    #    future.add_done_callback(lambda f: print("hello world!"))
-    #    return future
+    def _handle_verify_result(self, status, doms, at_enc):
+        print("VERIFY HANDLER", status)
+        if status == Verifier.Result.UNKNOWN:
+            assert doms is not None
+            assert at_enc is not None
+            f = self._client.submit(DistributedVerifier._split_fun,
+                    doms, at_enc, self._branch_factor)
+            return [f]
+        return []
 
-    #def _handle_split_result(future):
-    #    print("hello world! no pid?", os.getpid() % 10)
-    #    futures = []
-    #    print("hoi", os.getpid()% 10)
-    #    for doms, at_enc in future.result():
-    #        print("hello?")
-    #        print(future, doms)
-    #        #f = self._client.submit(DistributedVerifier._verify_fun,
-    #        #        self._verifier_factory, doms, at_enc
-    #        #futures.append(f)
-    #    return futures
-
-    #def _handle_verify_result(status):
-    #    pass
-
-    #def _split_and_verify(self, addtree, domains=None):
-    #    sp = SearchSpace(self.addtree)
-    #    sp.split(self._branch_factor)
-
-    #    futures = []
-    #    for leaf_id in sp.leafs():
-    #        addtree = sp.get_pruned_addtree(leaf_id).to_json()
-    #        domains = sp.get_domains(leaf_id)
-    #        future = self.client.submit(DistributedVerifier._solve,
-    #                self._verifier_factory,
-    #                leaf_id, domains, addtree, 60)
-    #        futures.append(future)
-
-    #    for future in as_completed(futures):
-    #        y = future.result()
-    #        print("RESULT as_completed", y, socket.gethostname(), os.getpid(), "done")
-
-    def _split_fun(domains, addtree, branch_factor):
-        addtree = DistributedVerifier._dec_at(addtree)
-        if domains is None: sp = SearchSpace(addtree)
-        else:               sp = SearchSpace(addtree, domains)
+    def _split_fun(domains, at_enc, branch_factor):
+        at = DistributedVerifier._dec_at(at_enc)
+        if domains is None: sp = SearchSpace(at)
+        else:               sp = SearchSpace(at, domains)
 
         sp.split(branch_factor)
 
-        results = []
+        subprobs = []
         for leaf_id in sp.leafs():
-            doms = sp.get_domains(leaf_id)
-            at = sp.get_pruned_addtree(leaf_id)
-            at = DistributedVerifier._enc_at(at)
-            results.append((doms, at))
+            sub_doms = sp.get_domains(leaf_id)
+            sub_at = sp.get_pruned_addtree(leaf_id)
+            sub_at_enc = DistributedVerifier._enc_at(sub_at)
+            subprobs.append((sub_doms, sub_at_enc))
 
-        return results
+        return DistributedVerifier.TaskType.SPLIT, subprobs
 
-    def _verify_fun(vfactory, domains, addtree, timeout):
-        addtree = DistributedVerifier._dec_at(addtree)
-        v = vfactory(domains, addtree)
+    def _verify_fun(vfactory, domains, at_enc, timeout):
+        at = DistributedVerifier._dec_at(at_enc)
+        v = vfactory(domains, at)
         v.set_timeout(timeout)
         status = v.verify() # maybe also return logs, stats
-        return status
+        return DistributedVerifier.TaskType.VERIFY, status, domains, at_enc
 
     def _enc_at(at):
         b = bytes(at.to_json(), encoding="ascii")
@@ -156,6 +96,3 @@ class DistributedVerifier:
     def _dec_at(b):
         at_json = codecs.decode(b, encoding="zlib").decode("ascii")
         return AddTree.from_json(at_json)
-
-
-
