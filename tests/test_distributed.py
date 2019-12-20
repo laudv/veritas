c550b7c6062cbae898d786f0a3e871e5e02228cf
Laurens Devos <laurens.devos@outlook.com>
2019-12-20 22:41:58 +0100

    distributed tests wip

--- tests/test_distributed.py
+++ tests/test_distributed.py
@@ -6,7 +6,7 @@ import importlib
 
 import treeck
 from treeck import *
-from treeck.verifier import Verifier, SplitCheckStrategy as Strategy
+from treeck.verifier import Verifier
 from treeck.z3backend import Z3Backend as Backend
 from treeck.distributed import DistributedVerifier
 
@@ -15,8 +15,8 @@ from start_dask import start_local
 
 class TestDistributedVerifier(unittest.TestCase):
     def test_img1(self):
-        def vfactory(doms, at):
-            v = Verifier(doms, at, Backend(), Strategy())
+        def vfactory(at, leaf):
+            v = Verifier(at, l0, Backend())
             v.add_constraint(v.fvar() < 0.0)
             v.add_constraint(v.xvar(0) > 50)
             return v
@@ -25,36 +25,37 @@ class TestDistributedVerifier(unittest.TestCase):
         #with start_local() as client:
             client.run(importlib.reload, treeck)
             at = AddTree.read("tests/models/xgb-img-easy.json")
-            dv = DistributedVerifier(client, at, vfactory)
-            dv.run()
+            st = SplitTree(at, {})
+            dv = DistributedVerifier(client, st, vfactory)
+            dv.check()
 
             #client.shutdown()
 
-    def test_adv(self):
-        instance_key = 0
-        model = "xgb-mnist-yis0-easy"
-        offset = 10
-        max_sum_offset = 50
+    #def test_adv(self):
+    #    instance_key = 0
+    #    model = "xgb-mnist-yis0-easy"
+    #    offset = 10
+    #    max_sum_offset = 50
 
-        def vfactory_aux(instance, offset, max_sum_offset, doms, at):
-            v = Verifier(doms, at, Backend(), Strategy())
-            sum_constraint = 0
-            for j, pixel in zip(range(v.num_features), instance):
-                x = v.xvar(j)
-                v.add_constraint((x > max(0, pixel-offset)) & (x < min(255, pixel+offset)))
-                sum_constraint += z3.If(x.get()-pixel <= 0, pixel-x.get(), x.get()-pixel)
-            v.add_constraint(sum_constraint < max_sum_offset)
-            return v
+    #    def vfactory_aux(instance, offset, max_sum_offset, doms, at):
+    #        v = Verifier(doms, at, Backend(), Strategy())
+    #        sum_constraint = 0
+    #        for j, pixel in zip(range(v.num_features), instance):
+    #            x = v.xvar(j)
+    #            v.add_constraint((x > max(0, pixel-offset)) & (x < min(255, pixel+offset)))
+    #            sum_constraint += z3.If(x.get()-pixel <= 0, pixel-x.get(), x.get()-pixel)
+    #        v.add_constraint(sum_constraint < max_sum_offset)
+    #        return v
 
-        with open("tests/models/mnist-instances.json") as f:
-            instance_key = 0
-            instance = np.array(json.load(f)[str(instance_key)])
+    #    with open("tests/models/mnist-instances.json") as f:
+    #        instance_key = 0
+    #        instance = np.array(json.load(f)[str(instance_key)])
 
-        vfactory = lambda doms, at: vfactory_aux(instance, offset, max_sum_offset, doms, at)
-        with Client("tcp://localhost:30333") as client:
-            at = AddTree.read(f"tests/models/{model}.json")
-            dv = DistributedVerifier(client, at, vfactory)
-            dv.run()
+    #    vfactory = lambda doms, at: vfactory_aux(instance, offset, max_sum_offset, doms, at)
+    #    with Client("tcp://localhost:30333") as client:
+    #        at = AddTree.read(f"tests/models/{model}.json")
+    #        dv = DistributedVerifier(client, at, vfactory)
+    #        dv.run()
 
 
 if __name__ == "__main__":
