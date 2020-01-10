0120e22a22b954b6674a7de0f0e37831967f8e29
Laurens Devos <laurens.devos@outlook.com>
2020-01-10 22:31:23 +0100

    bindings Tree::num_leafs

--- src/cpp/bindings.cpp
+++ src/cpp/bindings.cpp
@@ -115,6 +115,7 @@ PYBIND11_MODULE(pytreeck, m) {
         .def("root", [](const TreeRef& r) { return r.get().root().id(); })
         .def("index", [](const TreeRef& r) { return r.i; })
         .def("num_nodes", [](const TreeRef& r) { return r.get().num_nodes(); })
+        .def("num_leafs", [](const TreeRef& r) { return r.get().num_leafs(); })
         .def("is_root", [](const TreeRef& r, NodeId n) { return r.get()[n].is_root(); })
         .def("is_leaf", [](const TreeRef& r, NodeId n) { return r.get()[n].is_leaf(); })
         .def("is_internal", [](const TreeRef& r, NodeId n) { return r.get()[n].is_internal(); })
