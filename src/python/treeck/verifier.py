5f693f34f72371eceeabfb212f0774603a6c6215
Laurens Devos <laurens.devos@outlook.com>
2020-01-17 11:37:03 +0100

    bugfix: bounds can be inf when no leafs are reachable -> caused Z3 parse error

--- src/python/treeck/verifier.py
+++ src/python/treeck/verifier.py
@@ -419,8 +419,10 @@ class AddTreeInstance:
         self._v._backend.add_constraint(enc)
         lo, hi = self._v._lk.get_tree_bounds(self._instance_index, tree_index)
         wvar = self._wvars[tree_index]
-        self._v._backend.add_constraint(wvar >= lo)
-        self._v._backend.add_constraint(wvar <= hi)
+        if not math.isinf(lo):
+            self._v._backend.add_constraint(wvar >= lo)
+        if not math.isinf(hi):
+            self._v._backend.add_constraint(wvar <= hi)
 
     def add_all_trees(self):
         """ Add all trees in the addtree. """
