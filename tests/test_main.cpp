215a44e35051d0e63210ecd202275d300d4ab0ed
Laurens <laurens.devos@outlook.com>
2020-06-08 18:53:14 +0200

    test img

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -17,12 +17,12 @@ void test_simple()
     t = AddTree::TreeT();
     t.root().split(LtSplit(0, 1.52));
     t.root().left().split(BoolSplit(1));
-    t.root().left().left().set_leaf_value(1.0);
-    t.root().left().right().set_leaf_value(-1.0);
-    t.root().right().set_leaf_value(1.0);
+    t.root().left().left().set_leaf_value(4.0);
+    t.root().left().right().set_leaf_value(-4.0);
+    t.root().right().set_leaf_value(2.0);
     at.add_tree(std::move(t));
 
-    FeatInfo finfo(at, at, {}, true);
+    FeatInfo finfo(at, at, {1}, true);
 
     std::cout << at << std::endl;
     DomainStore store(finfo);
@@ -36,20 +36,41 @@ void test_simple()
         return true;
     };
 
-    //while (opt.step(box_filter, 0.0))
-    while (opt.step())
+    while (opt.step(box_filter, 0.0))
+    //while (opt.step())
     {
         std::cout << "================================ " << std::endl;
     }
 }
 
-void test_calhouse()
+void test_img()
 {
-    //auto file = "tests/models/xgb-calhouse-very-easy.json";
-    //std::shared_ptr<AddTree> at = std::make_shared<AddTree>(AddTree::from_json_file(file));
+    auto file = "tests/models/xgb-img-very-easy.json";
+    AddTree at = AddTree::from_json_file(file);
+    AddTree dummy;
+
+    //std::cout << at << std::endl;
+
+    FeatInfo finfo(at, dummy, {}, true); // minimize
+    DomainStore store(finfo);
+    KPartiteGraph g0(&store, at, finfo, 0);
+    KPartiteGraph dummyg(&store);
+    KPartiteGraphOptimize opt(g0, dummyg);
+
+    std::cout << g0 << std::endl;
+
+    auto box_filter = [](const DomainBox&) {
+        return true;
+    };
+
+    while (opt.step(box_filter, 50.4, 0.0))
+    {
+        std::cout << "================================ " << std::endl;
+    }
 }
 
 int main()
 {
-    test_simple();
+    //test_simple();
+    test_img();
 }
