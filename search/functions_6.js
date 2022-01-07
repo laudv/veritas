4257001892c0ddbd108fbdb366711aab65453eb6
laudv <laudv@users.noreply.github.com>
2022-01-07 15:17:51 +0000

    deploy: 13336eb5b419c7372a4a9fcdd556ced05b742f24

--- search/functions_6.js
+++ search/functions_6.js
@@ -3,8 +3,9 @@ var searchData=
   ['get_5fclosest_5fexample_0',['get_closest_example',['../group__python.html#gab5b0efdd58407da5a10ddee459aa9b23',1,'veritas::util']]],
   ['get_5fdomains_1',['get_domains',['../structveritas_1_1LtSplit.html#aac400d8b830751abfcc16701a918b4e0',1,'veritas::LtSplit']]],
   ['get_5felem_2',['get_elem',['../structveritas_1_1data.html#a2aaa60f9776ae02c1e892a139d300c2e',1,'veritas::data']]],
-  ['get_5fmax_5foutput_5fdifference_3',['get_max_output_difference',['../classveritas_1_1robustness_1_1RobustnessSearch.html#ab11fbb0a43aa92a120288f7484999b5f',1,'veritas.robustness.RobustnessSearch.get_max_output_difference()'],['../classveritas_1_1robustness_1_1VeritasRobustnessSearch.html#aa386998eac5856c1acd07592bb1f31e5',1,'veritas.robustness.VeritasRobustnessSearch.get_max_output_difference()'],['../classveritas_1_1robustness_1_1MilpRobustnessSearch.html#a1dda66962171e64b67107730d61b48d3',1,'veritas.robustness.MilpRobustnessSearch.get_max_output_difference()']]],
-  ['get_5fsplit_4',['get_split',['../classveritas_1_1NodeRef.html#a7ea39b85027c92c10be32f2c02e278bc',1,'veritas::NodeRef']]],
-  ['get_5fsplits_5',['get_splits',['../classveritas_1_1AddTree.html#a521fed3dd0d070fdfe81c9aab58de7fc',1,'veritas::AddTree']]],
-  ['get_5fvertices_6',['get_vertices',['../classveritas_1_1Graph.html#abda11d6a35e6fb92a4cc6251835487b5',1,'veritas::Graph']]]
+  ['get_5fleaf_5fids_3',['get_leaf_ids',['../classveritas_1_1NodeRef.html#a0c878658b07a092a10b33bfb2612f8ae',1,'veritas::NodeRef::get_leaf_ids() const'],['../classveritas_1_1NodeRef.html#a4e547b0a472f130f8517f12c421aad0e',1,'veritas::NodeRef::get_leaf_ids(std::vector&lt; NodeId &gt; &amp;ids) const'],['../classveritas_1_1Tree.html#a7a9582f0ab8d73a53fb74420a67a803d',1,'veritas::Tree::get_leaf_ids()']]],
+  ['get_5fmax_5foutput_5fdifference_4',['get_max_output_difference',['../classveritas_1_1robustness_1_1RobustnessSearch.html#ab11fbb0a43aa92a120288f7484999b5f',1,'veritas.robustness.RobustnessSearch.get_max_output_difference()'],['../classveritas_1_1robustness_1_1VeritasRobustnessSearch.html#aa386998eac5856c1acd07592bb1f31e5',1,'veritas.robustness.VeritasRobustnessSearch.get_max_output_difference()'],['../classveritas_1_1robustness_1_1MilpRobustnessSearch.html#a1dda66962171e64b67107730d61b48d3',1,'veritas.robustness.MilpRobustnessSearch.get_max_output_difference()']]],
+  ['get_5fsplit_5',['get_split',['../classveritas_1_1NodeRef.html#a7ea39b85027c92c10be32f2c02e278bc',1,'veritas::NodeRef']]],
+  ['get_5fsplits_6',['get_splits',['../classveritas_1_1AddTree.html#a521fed3dd0d070fdfe81c9aab58de7fc',1,'veritas::AddTree']]],
+  ['get_5fvertices_7',['get_vertices',['../classveritas_1_1Graph.html#abda11d6a35e6fb92a4cc6251835487b5',1,'veritas::Graph']]]
 ];
